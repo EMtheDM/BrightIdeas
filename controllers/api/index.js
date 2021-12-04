5cdd200f67cb27e5cdb12323cf88d04b20936fa1
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-12-04 15:25:38 -0600

    added commentRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,6 +4,7 @@ const projectRoutes = require('./projectRoutes');
 const projectAsksRoutes = require('./projectAsksRoutes');
 const projectListRoutes = require('./projectListRoutes');
 const taskRoutes = require('./taskRoutes');
+const commentRoutes = require('./commentRoutes');
 
 
 router.use('/users', userRoutes);
@@ -11,5 +12,6 @@ router.use('/projects', projectRoutes);
 router.use('/projectAsks', projectAsksRoutes);
 router.use('/projectLists', projectListRoutes);
 router.use('/tasks', taskRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
