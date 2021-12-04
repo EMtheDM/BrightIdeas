a3b592fd12f0aaf761e0546947dbbb6a8a3817bb
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-12-04 12:50:34 -0600

    Project model built

--- models/Project.js
+++ models/Project.js
@@ -1,2 +1,50 @@
-const { Model, Datatypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        client_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        client_email: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+        client_phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    }
+);
+
+module.exports = Project;
