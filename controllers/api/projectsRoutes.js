2d2a1ed19c7e2d9029d0f454927f75bf89edce99
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-12-08 20:54:03 -0600

    added withAuth to update and delete

--- controllers/api/projectsRoutes.js
+++ controllers/api/projectsRoutes.js
@@ -57,7 +57,7 @@ router.put('/projects/:id', withAuth, async (req, res) => {
 // then, for your LIST route (GET all), you filter out only the active projects, e.g. SELECT * FROM projects where DELETED is null
 
 // DELETE a project
-router.delete('/projects/:id', async (req, res) => {
+router.delete('/projects/:id', withAuth, async (req, res) => {
   try {
     const userData = await Project.destroy({
       where: {
