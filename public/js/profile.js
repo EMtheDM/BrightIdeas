75bc2bd94f9f2201058d98ac7dbc9884edf318fe
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-12-09 21:55:17 -0600

    updated createproject and delete project js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,50 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#project-list')
+    const name = document.querySelector('#project-name').value.trim();
+    const clientName = document.querySelector('#client-name').value.trim();
+    const clientEmail = document.querySelector('#client-email').value.trim();
+    const clientPhone = document.querySelector('#client-phone').value.trim();
+    const description = document.querySelector('#project-desc').value.trim();
+
+    if (name && clientName && description) {
+        const response = await fetch(`/api/projects`, {
+            method: 'POST',
+            body: JSON.stringify({ name, clientName, description }),
+            headers: {
+                'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/profile');
+    } else {
+        alert('Failed to create project!');
+    }
 }
+};
+
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/projects/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete project');
+        }
+    }
+};
+
+document
+    .querySelector('.project-list')
+    .addEventListener('click', delButtonHandler);
+
+document 
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
